79b9b531e1fe2eea8b9024ee2e7ca6f71104f91d
Tomas Kadavy <492979@mail.muni.cz>
2025-03-11 16:50:22 +0100

    Created skeletons for server and web

---
+++ civ-online-server/src/main/java/com/civ_online_server/CivOnlineServerApplication.java
@@ -0,0 +1,13 @@
+package com.civ_online_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivOnlineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivOnlineServerApplication.class, args);
+	}
+
+}
---
+++ civ-online-server/src/test/java/com/civ_online_server/CivOnlineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civ_online_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivOnlineServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
